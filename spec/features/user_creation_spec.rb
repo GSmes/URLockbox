04f06762c812a76e25a9284ad7677665157ee0af
Garrett <garrett.smestad@gmail.com>
2016-10-31 10:31:48 -0600

    Implement user creation

--- spec/features/user_creation_spec.rb
+++ spec/features/user_creation_spec.rb
@@ -6,27 +6,27 @@ RSpec.feature "Sign Up" do
 
     visit root_path
 
-    click_on "Sign Up"
+    click_on "Log In or Sign Up"
 
     expect(current_path).to eq(new_user_path)
 
-    within(".signup-form") do
-      fill_in "Email", with: user.email
-      fill_in "Password", with: user.password
-      fill_in "Password Confirmation", with: user.password
+    within("#signup-form") do
+      fill_in "email", with: user.email
+      fill_in "password", with: user.password
+      fill_in "password_confirmation", with: user.password
       click_button "Sign Up"
     end
 
     within ".alert" do
-      expect(page).to have_content("Successfully Logged In!")
+      expect(page).to have_content("Account created successfully!")
     end
 
     expect(current_path).to eq(root_path)
 
-    within(".navbar") do
+    within("#topnavbar") do
       expect(page).to have_content("Logged in as #{user.email}")
       expect(page).to have_content("Log Out")
-      expect(page).not_to have_content("Log In")
+      expect(page).not_to have_content("Log In or Sign Up")
     end
   end
 end
