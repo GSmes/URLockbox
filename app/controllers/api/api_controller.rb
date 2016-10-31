1717ed1382e01c290a3fb191f86ac608a4993d47
Garrett <garrett.smestad@gmail.com>
2016-10-31 17:21:13 -0600

    Fix protect_from_forgery errors & hopefully environment errors

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,4 +1,11 @@
-class Api::ApiController < ApplicationController
+class Api::ApiController < ActionController::Base
   protect_from_forgery with: :null_session
+
   respond_to :json
+
+  helper_method :current_user
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 end
